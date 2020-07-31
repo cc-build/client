e9ab02b33a697bef5311e947b4392a9883abf7db
Tensibai Zhaoying <tensibai@gmail.com>
2020-07-31 21:47:17 +0000

    Skip source control for hab tests

--- test/integration/source_spec.rb
+++ test/integration/source_spec.rb
@@ -1,16 +1,19 @@
-describe command 'bash -c "cd source/*/cinc/ && sha256sum -c cinc-[0-9]*.tar.xz.sha256sum"' do
-  its('exit_status') { should eq 0 }
-  its('stdout') { should match /OK$/ }
-end
-describe command 'bash -c "cd source/*/cinc/ && sha256sum -c cinc-full-[0-9]*.tar.xz.sha256sum"' do
-  its('exit_status') { should eq 0 }
-  its('stdout') { should match /OK$/ }
-end
-describe command 'bash -c "cd source/*/cinc/ && sha512sum -c cinc-[0-9]*.tar.xz.sha512sum"' do
-  its('exit_status') { should eq 0 }
-  its('stdout') { should match /OK$/ }
-end
-describe command 'bash -c "cd source/*/cinc/ && sha512sum -c cinc-full-[0-9]*.tar.xz.sha512sum"' do
-  its('exit_status') { should eq 0 }
-  its('stdout') { should match /OK$/ }
+control "Validate source package" do
+  only_if ENV['HAB_TEST'].nil?
+  describe command 'bash -c "cd source/*/cinc/ && sha256sum -c cinc-[0-9]*.tar.xz.sha256sum"' do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match /OK$/ }
+  end
+  describe command 'bash -c "cd source/*/cinc/ && sha256sum -c cinc-full-[0-9]*.tar.xz.sha256sum"' do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match /OK$/ }
+  end
+  describe command 'bash -c "cd source/*/cinc/ && sha512sum -c cinc-[0-9]*.tar.xz.sha512sum"' do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match /OK$/ }
+  end
+  describe command 'bash -c "cd source/*/cinc/ && sha512sum -c cinc-full-[0-9]*.tar.xz.sha512sum"' do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match /OK$/ }
+  end
 end
